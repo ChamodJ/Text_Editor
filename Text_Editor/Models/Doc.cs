1bc2c751e56588903118b7b25c352082b173f857
Chamod Perera <chamod914@gmail.com>
2024-07-04 23:01:44 +0530

    Add project files.

---
+++ Text_Editor/Models/Doc.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Text_Editor.Models
+{
+    public class Doc
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+
+        public IdentityUser User { get; set; }
+    }
+}
